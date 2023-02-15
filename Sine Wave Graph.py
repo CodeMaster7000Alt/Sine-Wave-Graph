f95f6ad7b063993147dc9350fb57f73d88524e0f
CodeMaster7000Alt <99122448+CodeMaster7000Alt@users.noreply.github.com>
2023-02-15 16:46:12 +0000

    Create Sine Wave Graph.py

---
+++ Sine Wave Graph.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(-np.pi,np.pi,100)
+y = np.sin(x)
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position('center')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+plt.plot(x,y, 'b-')
+plt.show()
